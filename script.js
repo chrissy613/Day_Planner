a5f076a0f2acb23990bdeaa103d39173786392a7
Chris <cec213@scarletmail.rutgers.edu>
2020-07-28 18:45:58 -0400

    css

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var d = new Date();
-document.getElementById("currentDay").innerHTML = d.toString();
+//DISPLAY DATE
+var date = new Date();
+document.getElementById("currentDay").innerHTML = date.toString();
+//INPUT TO LOCAL STORAGE
+var nineamInput = document.querySelector("#nineam");
+var nineamSubmit = document.querySelector("#nineamBtn");
+JSON.parse(localStorage.getItem("nineamInput")); 
+$("#nineamBtn").on("click", function(){
+    console.log(nineamInput.value);
+    localStorage.setItem("nineamInput", JSON.stringify(nineamInput.value));
+})
+
