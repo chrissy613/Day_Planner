89ba44d4906c3dec9d7a60fe8864a37030d0ee94
Chris <cec213@scarletmail.rutgers.edu>
2020-07-28 23:53:44 -0400

    latest ver

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ document.getElementById("currentDay").innerHTML = date.toString();
 //INPUT TO LOCAL STORAGE
 var nineamInput = document.querySelector("#nineam");
 var nineamSubmit = document.querySelector("#nineamBtn");
-JSON.parse(localStorage.getItem("nineamInput")); 
+var nineStorage = localStorage.getItem('nineamInput'); 
 $("#nineamBtn").on("click", function(){
     console.log(nineamInput.value);
-    localStorage.setItem("nineamInput", JSON.stringify(nineamInput.value));
+    localStorage.setItem("nineamInput", nineamInput.value);
 })
+var tenamInput = document.querySelector("#tenam");
+var tenamSubmit = document.querySelector("#tenamBtn");
+var tenStorage = localStorage.getItem('tenamInput'); 
+$("#tenamBtn").on("click", function(){
+    console.log(tenamInput.value);
+    localStorage.setItem("tenamInput", tenamInput.value);
+})
+
 
